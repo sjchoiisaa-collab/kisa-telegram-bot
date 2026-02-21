24722e537f8a81ef7aea007ec53e5ddcbbf64a40
sjchoiisaa-collab <sjchoi.isaa@gmail.com>
2026-02-21 16:18:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+import requests
 
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "🔥 KISA 보안 공지 테스트 메시지"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+payload = {
+    "chat_id": CHAT_ID,
+    "text": message
+}
+
+requests.post(url, data=payload)
